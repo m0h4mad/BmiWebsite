4fd591c6d9d8f58b4b1f11b6bd2172cafc840f29
Mohamad Reza <mohamadrezaganjkhani903@gmail.com>
2022-01-31 16:11:16 +0330

    Turned off the DEBUG Mode

--- bmiwebsite/settings.py
+++ bmiwebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1^r51kk82ibkjdckn6@1=3sl5zqyk%bc4b+-y%%&6^y&wiha+@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
